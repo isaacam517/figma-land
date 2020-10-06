3c73790dc74ea2000571848badfee591529b969d
isaac <isaacam517@gmail.com>
2020-10-06 10:08:06 -0300

    criada todas width e heightdas sessoes da aplicação 1

--- figma-land/src/App.js
+++ figma-land/src/App.js
@@ -5,17 +5,20 @@ import HeroLight from './components/HeroLigth';
 import LigthCTA from './components/LigthCTA';
 import PartnersLigth from './components/PartnerLigth';
 import PrincingLigth from './components/PricingLight';
+import ContactLigth from './components/ContactLigth';
+import FooterLigth from './components/FooterLigth';
 
 function App() {
   return (
     <div>
-     <HeaderLight />
-     <FeaturesLigth />
-     <HeroLight />
-     <LigthCTA />
-     <PartnersLigth />
-     <PrincingLigth />
-     
+      <HeaderLight />
+      <FeaturesLigth />
+      <HeroLight />
+      <LigthCTA />
+      <PartnersLigth />
+      <PrincingLigth />
+      <ContactLigth />
+      <FooterLigth />     
     </div>
   );
 }
