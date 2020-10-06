9939e10743c9d83364d4837d3b3389fe8522fa48
isaac <isaacam517@gmail.com>
2020-10-06 10:03:29 -0300

    criada todas width e heightdas sessoes da aplicação

---
+++ figma-land/src/components/ContactLigth/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components' 
+
+export const ContactContainer = styled.div`
+    width: 1440px;
+    height: 794.38px;
+    background-color: #D6EBFF;
+    color: white;
+`
---
+++ figma-land/src/components/FooterLigth/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FooterContainer } from './styles';
+
+const FooterLigth = () => {
+  return (
+    <FooterContainer>
+     FooterContainer
+    </FooterContainer>
+  );
+}
+
+export default FooterLigth;
